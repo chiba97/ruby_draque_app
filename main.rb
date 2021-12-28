47a3024bef1b0d94e3802fb26def9d247afdf9e2
chiba97 <ktfkyrya@gmail.com>
2021-12-28 04:51:09 +0000

    [Add]勇者の攻撃

--- main.rb
+++ main.rb
@@ -1,43 +1,39 @@
 class Brave
-  
-  attr_reader :name, :offence, :defence
+  attr_reader   :name, :offence, :defence
   attr_accessor :hp
-  
+
   def initialize(**params)
-    @name = params[:name]
-    @hp = params[:hp]
+    @name    = params[:name]
+    @hp      = params[:hp]
     @offence = params[:offence]
     @defence = params[:defence]
+  end
+
+  def attack(monster)
+    puts "#{@name}の攻撃"
+
+    damage = @offence - monster.defence
+    monster.hp -= damage
+
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
   end
 end
 
 class Monster
-  
-  attr_reader :name, :offence, :defence
+  attr_reader   :name, :offence, :defence
   attr_accessor :hp
-  
+
   def initialize(**params)
-    @name = params[:name]
-    @hp = params[:hp]
+    @name    = params[:name]
+    @hp      = params[:hp]
     @offence = params[:offence]
     @defence = params[:defence]
   end
 end
 
+brave = Brave.new(name: "勇者", hp: 500, offence: 150, defence: 100)
 monster = Monster.new(name: "スライム", hp: 250, offence: 200, defence: 100)
-puts <<~TEXT
-  NAME:#{monster.name}
-  HP:#{monster.hp}
-  OFFENCE:#{monster.offence}
-  DEFENCE:#{monster.defence}
-TEXT
+brave.attack(monster)
 
-
-brave = Brave.new(name: "koko", hp: 500, offence: 50, defence: 100)
-puts <<~TEXT
-  NAME:#{brave.name}
-  HP:#{brave.hp}
-  OFFENCE:#{brave.offence}
-  DEFENCE:#{brave.defence}
-TEXT
 

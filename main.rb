c157631f412dc8390912e7b8010c327a02f9edf4
chiba97 <ktfkyrya@gmail.com>
2021-12-28 04:11:38 +0000

    [Add]モンスタークラスの定義

--- main.rb
+++ main.rb
@@ -11,12 +11,33 @@ class Brave
   end
 end
 
-brave = Brave.new(name: "koko", hp: 500, offence: 50, defence: 100)
+class Monster
+  
+  attr_reader :name, :offence, :defence
+  attr_accessor :hp
+  
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offence = params[:offence]
+    @defence = params[:defence]
+  end
+end
 
+monster = Monster.new(name: "スライム", hp: 250, offence: 200, defence: 100)
 puts <<~TEXT
-NAME:#{brave.name}
-HP:#{brave.hp}
-OFFENCE:#{brave.offence}
-DEFENCE:#{brave.defence}
+  NAME:#{monster.name}
+  HP:#{monster.hp}
+  OFFENCE:#{monster.offence}
+  DEFENCE:#{monster.defence}
+TEXT
+
+
+brave = Brave.new(name: "koko", hp: 500, offence: 50, defence: 100)
+puts <<~TEXT
+  NAME:#{brave.name}
+  HP:#{brave.hp}
+  OFFENCE:#{brave.offence}
+  DEFENCE:#{brave.defence}
 TEXT
 
